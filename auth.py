0f626e5d0f3823d91adf01faac718590f2d3ed28
Arnav Gupta <arnavgupta9000@gmail.com>
2024-12-29 14:49:47 -0700

    log in/out is working completely now

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from __init__ import db
 from models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user
 
 auth = Blueprint('auth', __name__)
 
@@ -27,6 +27,12 @@ def login():
 
     return render_template('login.html')
 
+@auth.route('/logout')
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for('routes.home'))
+
 @auth.route('/register', methods = ['GET', 'POST'])
 
 def register():
@@ -40,11 +46,11 @@ def register():
 
         if user:
             flash("That user already exists, please log in")
-            render_template('register.html',user=current_user)
+            return render_template('login.html',user=current_user)
 
         if password != confirm_password:
             flash("Passwords are not the same")
-            render_template('register.html',user=current_user)
+            return render_template('register.html',user=current_user)
 
         new_user = User(email = email, password = generate_password_hash(password, method= 'pbkdf2:sha256', salt_length=8))
         db.session.add(new_user)
